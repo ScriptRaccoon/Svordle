7a5e933fcc2c3eb6d27f885af93d467fc4621e31
Script Raccoon <scriptraccoon@gmail.com>
2022-01-24 17:15:10 +0100

    bigger buttons on small screens

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -7,10 +7,10 @@
 
 <style>
     button {
-        --size: min(20px, 4vw);
+        --size: min(20px, 6vw);
         background: var(--color-button);
         font-size: var(--size);
-        padding: calc(0.5 * var(--size)) var(--size);
+        padding: calc(0.4 * var(--size)) calc(0.8 * var(--size));
         border-radius: 4px;
     }
 
