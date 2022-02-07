706e219c48606dff9773f07ab888becfc7906ef8
Script Raccoon <scriptraccoon@gmail.com>
2022-02-07 16:55:52 +0100

    file as comment

--- data/filter.py
+++ data/filter.py
@@ -1,6 +1,8 @@
 keys = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
         'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ä', 'ö', 'ü', 'ß')
 
+# generate german.txt from https://sourceforge.net/projects/germandict/files/
+
 input = open('german.txt', 'r', encoding='utf-8')
 output = open('output.js', 'w', encoding='utf-8')
 lines = input.readlines()
